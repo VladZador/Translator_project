2a5fe4fdc3de455a8308b28f8a82dbaa4ceb1504
Vladyslav Zadorozhnii <vladzador.ua@gmail.com>
2023-02-19 20:12:28 +0200

    Add editing of image placeholders

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 import requests
 import time
+import re
 from docx2python import docx2python
+from bs4 import BeautifulSoup
 
 # open the document
 docx_content = docx2python("Test.docx", html=True)
@@ -8,43 +10,86 @@ docx_content = docx2python("Test.docx", html=True)
 # save images
 docx_content.save_images('media')
 
+# parse through the content
+soup = BeautifulSoup(docx_content.text, "html.parser")
+text = soup.get_text()
 
-# translate the text by sending its parts to the Google Translator API
-translated_content = ""
-for content_part in docx_content.body[0][0][0]:
-    if content_part.isascii():
-        translated_content += content_part
+# break text into paragraphs and discards empty ones
+paragraphs = [p for p in text.splitlines() if p]
+
+# discard the image placeholders in text
+text_blocks = set([])
+for p in paragraphs:
+    if "----media/" not in p:
+        text_blocks.add(p)
     else:
-        url = f"https://translate.googleapis.com/translate_a/single?client=gtx&sl=uk&tl=en&dt=t&q={content_part}&ie=UTF-8&oe=UTF-8"
-        r = requests.get(url, timeout=(5, 5))
-        if r.json()[0]:
-            for item in r.json()[0]:
-                if item[0]:
-                    translated_content += item[0]
-        time.sleep(0.1)
+        text_blocks.update([block for block in p.split("----") if "media/image"
+                            not in block and not block.isascii()])
+
+# translate the text by sending its parts to the Google Translator API and
+# create translation table
+trans_table = []
+for block in text_blocks:
+    url = f"https://translate.googleapis.com/translate_a/single?client=gtx" \
+          f"&sl=uk&tl=en&dt=t&q={block}&ie=UTF-8&oe=UTF-8"
+    r = requests.get(url)
+    if r.json()[0]:
+        response = ""
+        for item in r.json()[0]:
+            if item[0]:
+                response += item[0]
+        trans_table.append((block, f" {response} "))
+    time.sleep(0.1)
+
+text = docx_content.text
+for trans_tuple in trans_table:
+    text = text.replace(trans_tuple[0], trans_tuple[1])
 
 
-# Correct extra whitespaces inside of tags
-content = translated_content.replace("< /", "</").replace("< s", "<s").replace("< p", "<p").replace("< b", "<b")
+# todo: 1) find a way to change the image placeholder into an actual image tag;
+#  2) Change the <span> tag to <p>
+
+
+# Find occurrences of a pattern like "----media/image1.png----
+#     Match characters "----media/" and then "----" literally
+#     Group (image\d\..{3}):
+#         "image" matches literally
+#         "\d" matches digits
+#             "\d+" matches digits, one or more occurrences
+#         "\." matches "."
+#         ".{3}" matches exactly 3 any characters
+regex_for_image = re.compile(r'----media/(image\d+\..{3})----')
+image_placeholders = set(regex_for_image.findall(docx_content.text))
+
+# Edit image placeholders
+# First, search for images inside text blocks (with incorrect <span> tags)
+for img_pl in image_placeholders:
+    if f'</span>----media/{img_pl}----<span style="font-size:28pt">' in text:
+        text.replace(
+            f'</span>----media/{img_pl}----<span style="font-size:28pt">',
+            f'<foo_bar_baz><img src="media/{img_pl}" alt="{img_pl}">'
+            f'</foo_bar_baz>')
+
+# Second, for other images
+for img_pl in image_placeholders:
+    if f'----media/{img_pl}----' in text:
+        text.replace(
+            f'----media/{img_pl}----',
+            f'<foo_bar_baz><img src="media/{img_pl}" alt="{img_pl}">'
+            f'</foo_bar_baz>')
 
 # Edit <span> html-tag to <p>
-content = content.replace("<span", "<p").replace("span>", "p>")
+text = text.replace("<span", "<p").replace("span>", "p>")
 
-
-# Edit text in order to include <img> tag where images should be.
-content_with_images = ""
-for elem in content.split("----"):
-    if "media/image" in elem:
-        elem = elem.replace("media/image", "")
-        elem = f'<span><img src="media/image{elem}" alt="{elem}"></span>'
-    content_with_images += elem
+# Edit dummy <foo_bar_baz> tags to <span>
+text = text.replace("foo_bar_baz>", "span>")
 
 
 # write a html file
 with open("Test.html", "w") as html_file:
-    html_file.write('<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8">'
-                    '<title>Translation</title></head><body>')
-    html_file.write(content_with_images)
+    html_file.write('<!DOCTYPE html><html lang="en"><head><meta charset='
+                    '"UTF-8"><title>Translation</title></head><body>')
+    html_file.write(text)
     html_file.write('</body></html>')
 
 # close the document
